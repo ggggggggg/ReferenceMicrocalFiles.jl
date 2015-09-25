6b1eff9d334d9acd983a5271ecedaa13d40bb79d
Galen O'Neil <galen.oneil@nist.gov>
2015-09-25 16:57:55 -0600

    file noise_filename assignment

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,4 @@ using Base.Test
 # write your own tests here
 @test 1 == 1
 @test isfile(ReferenceMicrocalFiles.dict["tupac_calibronium"].filename)
+@test isfile(ReferenceMicrocalFiles.dict["tupac_fe_emission"].noise_filename)

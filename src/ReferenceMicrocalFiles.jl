f6bda5a272ba41f9f6f859b5175abc91976a586c
Galen O'Neil <galen.oneil@nist.gov>
2015-09-25 16:16:42 -0600

    add a file, switch from function to dict for ljh paths

--- src/ReferenceMicrocalFiles.jl
+++ src/ReferenceMicrocalFiles.jl
@@ -1,10 +1,12 @@
 module ReferenceMicrocalFiles
 
-function getljhpaths()
-	datadir = normpath(joinpath( dirname(@__FILE__), "../ljh"))
-	fnames = filter(p->endswith(p,".ljh"), readdir(datadir))
-	[joinpath(datadir, fname) for fname in fnames]
-end
+const ljhdir = normpath(joinpath( dirname(@__FILE__), "../ljh"))
+
+const ljhpathdict = Dict(
+	"tupac_calibronium"=>joinpath(ljhdir, "20140516_tupac_calibronium_4_row_4hz_chan1.ljh"),
+	"tupac_fe_emission"=>joinpath(ljhdir, "20150730_50mM_irontris_100ps_xes/20150730_50mM_irontris_100ps_xes_chan1.ljh")
+					 )
+
 
 # package code goes here
 

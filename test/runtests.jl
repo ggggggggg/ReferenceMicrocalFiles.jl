abe55d1102d8a16e0404fe153e635df82f77f3ff
Galen O'Neil <galen.oneil@nist.gov>
2015-09-25 16:51:30 -0600

    add noise file, change structure to dict

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
-using ReferenceLJHFiles
+using ReferenceMicrocalFiles
 using Base.Test
 
 # write your own tests here
 @test 1 == 1
+@test isfile(ReferenceMicrocalFiles.dict["tupac_calibronium"].filename)
